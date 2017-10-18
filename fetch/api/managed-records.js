b884f5c4933ab3b195ae226c63005b08832a89b4
Ian Hill <ian@parlinet.com>
2017-10-18 15:13:43 -0400

    Finish Ad Hoc homework

--- fetch/api/managed-records.js
+++ fetch/api/managed-records.js
@@ -6,4 +6,63 @@ window.path = "http://localhost:3000/records";
 
 // Your retrieve function plus any additional functions go here ...
 
+const constants = {
+  PAGE_SIZE: 10,
+  MAX_PAGE: 50,
+  PRIMARY_COLORS: ['red', 'blue', 'yellow'],
+};
+
+function retrieve(options) {
+  const page = (options && options.page) ? options.page : 1;
+  const colors = (options && options.colors) ? options.colors : [];
+  const offset = (page - 1) * constants.PAGE_SIZE;
+  const uri = URI(window.path)
+      .addSearch('limit', constants.PAGE_SIZE)
+      .addSearch('offset', offset)
+      .addSearch({ 'color[]': colors });
+  return fetch(uri.toString())
+  .then((response) => {
+    if (response.status !== 200) {
+      throw new Error('Retrieve failed.')
+    }
+    return response.json();
+  })
+  .then((items) => {
+    // Map array of item ids
+    const ids = items.map(item => item.id);
+    // Bin items by disposition and assign isPrimary
+    const { open, closed } = items.reduce((acc, item) => {
+      const newItem = Object.assign({}, item, {
+        isPrimary: constants.PRIMARY_COLORS.indexOf(item.color) > -1,
+      });
+      if (newItem.disposition === 'open') {
+        return { open: acc.open.concat([newItem]), closed: acc.closed };
+      }
+      return { open: acc.open, closed: acc.closed.concat([newItem]) };
+    }, { open: [], closed: [] });
+    // Count items with closed disposition and primary color
+    const closedPrimaryCount =
+        closed.reduce((acc, item) => acc + (item.isPrimary ? 1 : 0), 0);
+    // Set pages and resolve
+    return Promise.resolve({
+      ids,
+      open,
+      closedPrimaryCount,
+      previousPage: page > 1 ? page - 1 : null,
+      nextPage: (page < constants.MAX_PAGE &&
+          items.length >= constants.PAGE_SIZE) ? page + 1 : null,
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+    return Promise.resolve({
+      ids: [],
+      open: [],
+      closedPrimaryCount: 0,
+      previousPage: null,
+      nextPage: null,
+    });
+  });
+}
+
 export default retrieve;
--- hhbuilder/index.js
+++ hhbuilder/index.js
@@ -1,1 +1,158 @@
 // your code goes here ...
+
+// Get handles to DOM elements -------------------------------------------------
+var form = document.getElementsByTagName('form')[0];
+var list = document.getElementsByClassName('household')[0];
+var addButton = document.getElementsByClassName('add')[0];
+var debug = document.getElementsByClassName('debug')[0];
+var previousWarning;
+
+
+// Define initial state --------------------------------------------------------
+var householdMembers = [];
+var uniqueKeyCounter = 0;
+
+
+// Constants -------------------------------------------------------------------
+var constants = {
+  AGE_TITLE: 'Age: ',
+  REL_TITLE: 'Relationship: ',
+  SMOKER_TITLE: 'Smoker: ',
+  RELATIONSHIP_MAP: {
+    self: 'Self',
+    spouse: 'Spouse',
+    child: 'Child',
+    parent: 'Parent',
+    grandparent: 'Grandparent',
+    other: 'Other',
+  },
+  WARNING_PREFACE: 'Sorry! We were unable to create a new household member:',
+  AGE_WARNING: 'Age is required. Please enter a number greater than 0.',
+  REL_WARNING: 'Relationship is required. Please select a relationship.',
+  REMOVE_BUTTON: 'Remove',
+};
+
+
+// Helper functions ------------------------------------------------------------
+
+function removeHouseholdMember(key) {
+  // Remove member from DOM
+  var memberToBeRemoved = document.getElementById('householdmember-' + key);
+  list.removeChild(memberToBeRemoved);
+  // Remove member from state
+  householdMembers = householdMembers.filter(function(member) {
+    return member.key !== key;
+  });
+}
+
+function createMemberElement(member) {
+  // Build household member properties
+  var ageProperty = document.createElement('p');
+  ageProperty.innerText = constants.AGE_TITLE + member.age;
+  var relProperty = document.createElement('p');
+  relProperty.innerText =
+      constants.REL_TITLE + constants.RELATIONSHIP_MAP[member.rel];
+  var smokerProperty = document.createElement('p');
+  smokerProperty.innerText =
+      constants.SMOKER_TITLE + (member.smoker ? 'Yes' : 'No');
+  var removeButton = document.createElement('button');
+  removeButton.innerText = constants.REMOVE_BUTTON;
+  removeButton.onclick = function() {
+    removeHouseholdMember(member.key);
+  }
+  // Assemble list item
+  var newMember = document.createElement('li');
+  newMember.id = 'householdmember-' + member.key;
+  newMember.appendChild(ageProperty);
+  newMember.appendChild(relProperty);
+  newMember.appendChild(smokerProperty);
+  newMember.appendChild(removeButton);
+  return newMember;
+}
+
+function createWarningItem(message) {
+  var warning = document.createElement('li');
+  var warningBody = document.createElement('p');
+  warningBody.innerText = message;
+  warning.appendChild(warningBody);
+  return warning;
+}
+
+function createWarningList(items) {
+  var warningDiv = document.createElement('div');
+  var warningPreface = document.createElement('p');
+  warningPreface.innerText = constants.WARNING_PREFACE;
+  var warningList = document.createElement('ul');
+  for (var i = 0; i < items.length; i++) {
+    warningList.appendChild(items[i]);
+  }
+  warningDiv.appendChild(warningPreface);
+  warningDiv.appendChild(warningList);
+  return warningDiv;
+}
+
+function validateOrCreateWarningElement(member) {
+  var warnings = [];
+  // Validate or create warning list items
+  if (!(parseInt(member.age) > 0)) {
+    warnings.push(createWarningItem(constants.AGE_WARNING));
+  }
+  if (!member.rel) {
+    warnings.push(createWarningItem(constants.REL_WARNING));
+  }
+  // Create warning list if field(s) were invalid
+  if (warnings.length) {
+    return createWarningList(warnings);
+  }
+  return false;
+}
+
+
+// Root functions --------------------------------------------------------------
+
+function addHouseholdMember(event) {
+  event.preventDefault();
+  var member = {
+    key: uniqueKeyCounter,
+    age: parseInt(form.age.value),
+    rel: form.rel.value,
+    smoker: form.smoker.checked,
+  };
+  uniqueKeyCounter += 1;
+  var warning = validateOrCreateWarningElement(member);
+  if (warning) {
+    if (previousWarning) {
+      form.replaceChild(warning, previousWarning);
+    } else {
+      form.appendChild(warning);
+    }
+    previousWarning = warning;
+  } else {
+    householdMembers.push(member);
+    list.appendChild(createMemberElement(member));
+    form.reset();
+    if (previousWarning) {
+      form.removeChild(previousWarning);
+      previousWarning = null;
+    }
+  }
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  var data = JSON.stringify({
+    members: householdMembers,
+  });
+  // Display data in debug block
+  debug.innerText = data;
+  debug.style.display = 'block';
+  // Send data to server as JSON
+  var request = new XMLHttpRequest();
+  request.open('POST', 'https://localhost/api/household/save');
+  request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+  request.send(data);
+}
+
+// Configure elements ----------------------------------------------------------
+addButton.onclick = addHouseholdMember;
+form.onsubmit = submitForm;
